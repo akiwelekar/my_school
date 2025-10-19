ea7fc2697501a224c0e4dd65949ba5c1d22c97f5
ARVIND kIWELEKAR <akiwelekar@gmail.com>
2025-10-19 20:29:49 +0530

    Fix publisher variable name in hooks.py

--- my_school/hooks.py
+++ my_school/hooks.py
@@ -1,6 +1,6 @@
 app_name = "my_school"
 app_title = "My School"
-publisher = "ERP-CSE-DBATU"
+app_publisher = "ERP-CSE-DBATU"
 app_description = "Custom Education extensions"
 app_email = "akiwelekar@gmail.com"
 app_license = "MIT"
