1fef38dde318d5fe46dc28d21a25c73ea302328f
ARVIND kIWELEKAR <akiwelekar@gmail.com>
2025-10-19 19:58:58 +0530

    Update publisher and email in hooks.py

--- my_school/hooks.py
+++ my_school/hooks.py
@@ -1,16 +1,9 @@
 app_name = "my_school"
 app_title = "My School"
-<<<<<<< HEAD
-app_publisher = "Your Dept"
-app_description = "Custom Education extensions"
-app_email = "you@example.com"
-=======
-app_publisher = "ERP-CSE-DBATU"
+publisher = "ERP-CSE-DBATU"
 app_description = "Custom Education extensions"
 app_email = "akiwelekar@gmail.com"
->>>>>>> 615c646b3cc6f17ddcf2bd78bb2e8dd7936d992b
 app_license = "MIT"
-
 role_home_page = {
     "Student": "/student-dashboard",
     "Instructor": "/teacher-dashboard",
