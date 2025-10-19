2190bc50f2066a8e96ae7a5ae10c40c46940ac22
ARVIND kIWELEKAR <akiwelekar@gmail.com>
2025-10-19 17:16:09 +0530

    Add hooks.py for My School app configuration

---
+++ my_school/hooks.py
@@ -0,0 +1,11 @@
+app_name = "my_school"
+app_title = "My School"
+app_publisher = "ERP-CSE-DBATU"
+app_description = "Custom Education extensions"
+app_email = "akiwelekar@gmail.com"
+app_license = "MIT"
+
+role_home_page = {
+    "Student": "/student-dashboard",
+    "Instructor": "/teacher-dashboard",
+}
